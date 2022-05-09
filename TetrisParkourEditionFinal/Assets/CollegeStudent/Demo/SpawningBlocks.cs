750a5dfce742b3c29f64065f8dcf0e13cec4441e
Kim <607762@student.dusd.net>
2022-05-09 12:52:04 -0700

    Falling Blocks
    I have added the gameobject called Spawner.

--- TetrisParkourEditionFinal/Assets/CollegeStudent/Demo/SpawningBlocks.cs
+++ TetrisParkourEditionFinal/Assets/CollegeStudent/Demo/SpawningBlocks.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class SpawningBlocks : MonoBehaviour
 {
+    public GameObject[] Tetrominoes;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        NewTetromino();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NewTetromino()
     {
-        
+        Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
     }
 }
