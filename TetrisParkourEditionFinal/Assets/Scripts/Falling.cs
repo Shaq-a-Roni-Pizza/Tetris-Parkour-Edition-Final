bd57effa508709ba5c2228a24b5847a54893cea0
Kim <607762@student.dusd.net>
2022-05-10 12:30:05 -0700

    The Scripts
    I have attached the scripts to the blocks.

---
+++ TetrisParkourEditionFinal/Assets/Scripts/Falling.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling : MonoBehaviour
+{
+    private float previousTime;
+    public float fallTime = 0.8f;
+    public static int height = 20;
+    public static int width = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - previousTime > fallTime)
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+                this.enabled = false;
+                FindObjectOfType<SpawningBlocks>().NewTetromino();
+            }
+            previousTime = Time.time;
+        }
+    }
+
+    bool ValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if(roundedX < 0 || roundedX >= width || roundedY < 0 ||roundedY >= height)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
