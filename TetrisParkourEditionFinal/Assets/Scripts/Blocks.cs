a3253b26fac047fddc284291c1e5cc808a677a3b
Kim <607762@student.dusd.net>
2022-05-12 15:58:36 -0700

    Updated Scripts 3
    I have update the scripts for the third time.

--- TetrisParkourEditionFinal/Assets/Scripts/Blocks.cs
+++ TetrisParkourEditionFinal/Assets/Scripts/Blocks.cs
@@ -6,8 +6,6 @@ public class Blocks : MonoBehaviour
 {
     private float previousTime;
     public float fallTime = 0.8f;
-    public static int height = 20;
-    public static int width = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -18,55 +16,10 @@ public class Blocks : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F))
-        {
-            transform.position += new Vector3(-1, 0, 0);
-            if (!ValidMove())
-                transform.position -= new Vector3(-1, 0, 0);
-        }
-        else if (Input.GetKeyDown(KeyCode.H))
-        {
-            transform.position += new Vector3(1, 0, 0);
-            if (!ValidMove())
-                transform.position -= new Vector3(-1, 0, 0);
-        }
-
-        if(Time.time - previousTime > (Input.GetKey(KeyCode.G) ? fallTime / 10 : fallTime))
+        if(Time.time - previousTime > fallTime)
         {
             transform.position += new Vector3(0, -1, 0);
-            if (!ValidMove())
-            {
-                transform.position -= new Vector3(0, -1, 0);
-                this.enabled = false;
-                FindObjectOfType<SpawningBlocks>().NewTetromino();
-            }
             previousTime = Time.time;
-        }
-    }
-
-    bool ValidMove()
-    {
-        foreach (Transform children in transform)
-        {
-            int roundedX = Mathf.RoundToInt(children.transform.position.x);
-            int roundedY = Mathf.RoundToInt(children.transform.position.y);
-
-            if(roundedX < 0 || roundedX >= width || roundedY < 0 ||roundedY >= height)
-            {
-                return false;
-            }
-        }
-
-        return true;
-
-        void OnCollisionEnter(Collision2D col)
-        {
-            if (col.gameObject.name == "ground")
-            {
-                Debug.Log("Collision Dectected");
-            }
-
-      
         }
     }
 }
--- TetrisParkourEditionFinal/Assets/Scripts/SpawningBlocks.cs
+++ TetrisParkourEditionFinal/Assets/Scripts/SpawningBlocks.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class SpawningBlocks : MonoBehaviour
 {
-    public GameObject[] Tetrominoes;
+    public GameObject Tetromino;
     float randX;
-    Vector2 wheretoSpawn;
+    Vector2 whereToSpawn;
     public float spawnRate = 2f;
     float nextSpawn = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        NewTetromino();
+
     }
 
-    public void NewTetromino()
+    void Update()
     {
-        Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(0f, 0f);
+            whereToSpawn = new Vector2(randX, transform.position.y);
+            Instantiate(Tetromino, whereToSpawn, Quaternion.identity);
+        }
     }
 }
