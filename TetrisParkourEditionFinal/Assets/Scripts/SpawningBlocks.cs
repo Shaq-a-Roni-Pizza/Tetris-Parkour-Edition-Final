cbd150fa8c7147d0f95c932cf67b01068e2ff930
Benji Kim <607762@student.dusd.net>
2022-05-12 22:04:49 -0700

    Script spawning finished
    I have finally finished the spawning script.

--- TetrisParkourEditionFinal/Assets/Scripts/SpawningBlocks.cs
+++ TetrisParkourEditionFinal/Assets/Scripts/SpawningBlocks.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class SpawningBlocks : MonoBehaviour
 {
-    public GameObject Tetromino;
-    float randX;
-    Vector2 whereToSpawn;
-    public float spawnRate = 2f;
-    float nextSpawn = 0.0f;
+    public GameObject block1, block2, block3, block4, block5, block6, block7;
+    public float spawnRate = 7f;
+    float nextSpawn = 0f;
+    int whatToSpawn;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +19,33 @@ public class SpawningBlocks : MonoBehaviour
     {
         if (Time.time > nextSpawn)
         {
+            whatToSpawn = Random.Range(1, 6);
+
+            switch (whatToSpawn)
+            {
+                case 1: 
+                    Instantiate(block1, transform.position, Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(block2, transform.position, Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(block3, transform.position, Quaternion.identity);
+                    break;
+                case 4:
+                    Instantiate(block4, transform.position, Quaternion.identity);
+                    break;
+                case 5:
+                    Instantiate(block5, transform.position, Quaternion.identity);
+                    break;
+                case 6:
+                    Instantiate(block6, transform.position, Quaternion.identity);
+                    break;
+                case 7:
+                    Instantiate(block7, transform.position, Quaternion.identity);
+                    break;
+            }
             nextSpawn = Time.time + spawnRate;
-            randX = Random.Range(0f, 0f);
-            whereToSpawn = new Vector2(randX, transform.position.y);
-            Instantiate(Tetromino, whereToSpawn, Quaternion.identity);
         }
     }
 }
